f42a0895a5b101ee0ece728ce85f2dd12f5b721d
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-01-23 22:56:33 -0500

    add note aboute typescript knowing about index support

--- sort/build/index.js
+++ sort/build/index.js
@@ -7,11 +7,16 @@ var Sorter = /** @class */ (function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
+                // if collection is an array of numbers
                 if (this.collection[j] > this.collection[j + 1]) {
                     var leftHand = this.collection[j];
+                    // TS gives an error message because it knows that 
+                    // you can read but not write at collection[index]
+                    // for a string
                     this.collection[j] = this.collection[j + 1];
                     this.collection[j + 1] = leftHand;
                 }
+                // if collection is a string
             }
         }
     };
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,16 +1,23 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
+
+        // if collection is an array of numbers
         if (this.collection[j] > this.collection[j + 1]) {
           const leftHand = this.collection[j];
+          // TS gives an error message because it knows that 
+          // you can read but not write at collection[index]
+          // for a string
           this.collection[j] = this.collection[j+1];
           this.collection[j+1] = leftHand;
         }
+
+        // if collection is a string
       }
     }
   }
