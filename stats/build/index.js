287a11f674a9aaa25b814027a768fc88ac44289f
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-01-30 08:58:24 -0500

    add draw constant

--- stats/build/index.js
+++ stats/build/index.js
@@ -14,6 +14,7 @@ var matches = fs_1.default
 });
 var homeWin = 'H';
 var awayWin = 'A';
+var draw = 'D';
 var manUnitedWins = 0;
 for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
     var match = matches_1[_i];
