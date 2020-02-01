1def61e8d3c57501ebef03cb5de78eeefefedc32
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-02-01 10:40:38 -0500

    add ability to trigger functions from eventing system

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,6 +3,13 @@ import { User } from './models/User';
 const user = new User({ name: 'myname', age: 20 });
 
 user.on('change', () => {
+  console.log('change1')
+});
+user.on("change", () => {
+  console.log("change2");
+});
+user.on("save", () => {
+  console.log("save");
 });
 
-console.log(user)
+user.trigger('change')
