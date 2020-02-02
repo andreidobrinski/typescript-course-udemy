d9b16bfe841e30e38fb38a48ba1ebae82094ed60
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-02-01 20:03:47 -0500

    add ability to fetch for user

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,15 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
+const user = new User({ id: 1 });
 
-user.on('change', () => {
-  console.log('change1')
-});
-user.on("change", () => {
-  console.log("change2");
-});
-user.on("save", () => {
-  console.log("save");
-});
-
-user.trigger('change')
+user.fetch();
