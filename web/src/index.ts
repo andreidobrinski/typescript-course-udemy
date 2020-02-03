1bd2cdc81bbd28aff67f27b2a0e5654256e986a2
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-02-03 15:45:51 -0500

    add user.set method

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,4 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
-console.log(user.get("name"));
+user.on('change', () => {
+  console.log('user was changed')
+});
+
+user.set({ name: 'new namee' });
