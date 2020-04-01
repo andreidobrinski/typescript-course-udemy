b8c0912908f2aac694a38951684290a832440dbe
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-04-01 13:53:12 -0400

    start on mapStateToProps

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
-export class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodo(): any;
+}
+
+export class App extends React.Component<AppProps> {
   render() {
     return <div>hello</div>;
   }
 }
+
+const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
+  return { todos: state.todos };
+};
